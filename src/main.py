9bdd37a0b912f5eac7a5bd8e0be8221a4fb1b6d9
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-08 11:56:32 -0300

    refac/ uncomment scheduler

--- src/main.py
+++ src/main.py
@@ -1,23 +1,14 @@
 from prometheus_fastapi_instrumentator import Instrumentator
-
 from uvicorn import run
-from src.app import app
+
 from src.scheduler import scheduler
+from src.app import app
 
 # Start Prometheus logging metrics
 Instrumentator().instrument(app).expose(app)
 
-# # Start the scheduler
-# scheduler.start()
-
-'''
-from fastapi_jwt_auth import AuthJWT
-
-# callback to get your configuration
-@AuthJWT.load_config
-def get_config():
-    return Settings()
-'''
+# Start the scheduler
+scheduler.start()
 
 # Run the applications
 if __name__ == "__main__":
