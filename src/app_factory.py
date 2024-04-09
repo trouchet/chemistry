c3672cb3c8a5f406472d950d2b7af9cee6bc5342
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-09 18:57:22 -0300

    feat/ CORS mddleware

--- src/app_factory.py
+++ src/app_factory.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-from src.routes import setup, auth, recommendation
+from src.routes import setup, signup, auth, recommendation
 
 def create_app():
     app_ = FastAPI()
-    
+
     app_.include_router(setup.router, prefix="/api", tags=["setup"])
     app_.include_router(auth.router, prefix="/api")
     app_.include_router(
         recommendation.router, 
         prefix="/api/recommendation", tags=["recommendation"]
     )
+    app_.include_router(signup.router, prefix="/api", tags=["recommendation"])
+
+    app_.add_middleware(CORSMiddleware)
 
     return app_
