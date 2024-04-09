948aed8efa1603d5bed68b807cf75a50302bae1f
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-09 19:03:11 -0300

    feat/ custom middlewares

---
+++ src/custom_middlewares.py
@@ -0,0 +1,13 @@
+from starlette.requests import Request
+from starlette.middleware.base import BaseHTTPMiddleware
+import logging
+from colorama import Fore, Style
+
+'''
+from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
+from starlette.middleware.gzip import GZipMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+'''
