05cbfaea95663695f04875986089c8dcb2cbaddc
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-11 19:12:43 -0300

    hotfix/ remove app factory and move to app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,25 @@
-from src.app_factory import create_app
+# Description: This file is responsible for creating the 
+# FastAPI app instance and adding the routes to it.
+from fastapi import FastAPI
+from starlette.middleware.cors import \
+    CORSMiddleware
+
+from src.routes import setup, signup, auth, recommendation
+
+
+def create_app():
+    app_ = FastAPI()
+
+    app_.include_router(setup.router, prefix="/api", tags=["setup"])
+    app_.include_router(auth.router, prefix="/api")
+    app_.include_router(
+        recommendation.router, prefix="/api/recommendation", tags=["recommendation"]
+    )
+    app_.include_router(signup.router, prefix="/api", tags=["recommendation"])
+
+    app_.add_middleware(CORSMiddleware)
+
+    return app_
 
 # Get the number of applications from the environment variable
 app = create_app()
