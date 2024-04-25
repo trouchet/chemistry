5904c3610c6def530b253965f379cf759b7cf5e1
Bruno Peixoto <brunolnetto@gmail.com>
2024-04-25 09:41:11 -0300

    refac/ assure scheduler to rnu on separated thread

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import threading
 from prometheus_fastapi_instrumentator import Instrumentator
 from uvicorn import run
 
@@ -7,9 +8,14 @@ from src.app import app
 # Start Prometheus logging metrics
 Instrumentator().instrument(app).expose(app)
 
-# Start the scheduler
-scheduler.start()
+# Start the scheduler in a separate thread
+def start_scheduler():
+    scheduler.start()
 
-# Run the applications
+scheduler_thread = threading.Thread(target=start_scheduler)
+scheduler_thread.daemon = True 
+scheduler_thread.start()
+
+# Run the application
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=8000)
