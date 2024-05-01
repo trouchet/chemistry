ebafac75d7bfad7a581dd651a4a4644b848128a7
Bruno Peixoto <brunolnetto@gmail.com>
2024-05-01 20:24:23 -0300

    config/ add migrate script to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-set -exo pipefail
-
-echo "Waiting for Postgres..."
-
-while ! nc -z postgres 5432; do
-	sleep 0.1
-done
-
-echo "Postgres started"
-
+python src/database/migrate.py
 uvicorn api.app:app --reload --host "0.0.0.0" --port "8000"
