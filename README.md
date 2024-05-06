858215b0cf25cc0465243df1123869bbff995254
Bruno Peixoto <brunolnetto@gmail.com>
2024-05-05 21:02:50 -0300

    refac/ refactored configuration, new README and environment

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 [![Python Version](https://img.shields.io/badge/Python-3.7%20|%203.8%20|%203.9-blue)](https://www.python.org/downloads/)
 [![FastAPI Version](https://img.shields.io/badge/FastAPI-0.68.1-blue)](https://fastapi.tiangolo.com/)
 [![PostgreSQL Version](https://img.shields.io/badge/PostgreSQL-13-blue)](https://www.postgresql.org/)
-[![Redis Version](https://img.shields.io/badge/Redis-6.2-blue)](https://redis.io/)
-[![Prometheus Version](https://img.shields.io/badge/Prometheus-2.30-blue)](https://prometheus.io/)
 
-This is a minimal repository for a FastAPI app instance with postgres and redis database, prometheus monitoring.
+
+This is a minimal repository for a FastAPI app instance with: 
+    - Database: _postgres_;
+    - ORM: _SQLAlchemy_;
+    - Monitoring: _prometheus_.
 
 ## Get started
 
@@ -19,9 +21,14 @@ For installation of packages on file `requirements.txt`, run the command on term
 2. API references
     Tools: FastAPI: https://fastapi.tiangolo.com/
     	
-## Deploy - Under construction
+## Deployment
 
-Run command `docker compose up`
+1. Change the environment variables on `.env` file;
+    a. Change variable ENVIRONMENT to `production`;
+    b. Change the non-test database credentials to your remote database configuration;
+    c. Change the variable `SECRET_KEY` to some trustworthy string;
+
+2. Run command `docker compose up`;
 
 ## Build and Tests
 
@@ -29,6 +36,6 @@ Under construction
 
 ## TODO
 
-[ ] (Normal) Validate data insertion on database;
-[ ] (Hard) Create endpoint for data import;
-[ ] (Hard) Create background periodic task for data processing (e.g. Apscheduler or Celery);
+[ ] ( Normal ) Validate data insertion on database;
+[ ] (  Hard  ) Create endpoint for data import;
+[ ] (  Hard  ) Create routine for data processing (alternatives: Apscheduler/Celery).
--- src/config.py
+++ src/config.py
@@ -15,21 +15,86 @@ APP_PORT = environ.get("APP_PORT", 8000)
 
 ENVIRONMENT = environ.get("ENVIRONMENT", "development")
 
+# Postgres settings
+POSTGRES_ENGINE = "postgresql+asyncpg"
+
 POSTGRES_DB = environ.get("POSTGRES_DB", "postgres")
 POSTGRES_HOST = environ.get("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = environ.get("POSTGRES_PORT", 5432)
 POSTGRES_USER = environ.get("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = environ.get("POSTGRES_PASSWORD", "postgres")
+
+POSTGRES_DB_TEST = environ.get("POSTGRES_DB_TEST", "postgres")
+POSTGRES_HOST_TEST = environ.get("POSTGRES_HOST_TEST", "localhost")
+POSTGRES_PORT_TEST = environ.get("POSTGRES_PORT_TEST", 5435)
+POSTGRES_USER_TEST = environ.get("POSTGRES_USER_TEST", "postgres")
+POSTGRES_PASSWORD_TEST = environ.get("POSTGRES_PASSWORD_TEST", "postgres")
+
 POSTGRES_ECHO = environ.get("POSTGRES_ECHO", "false")
 POSTGRES_POOL_SIZE = environ.get("POSTGRES_POOL_SIZE", 10)
 
 SECRET_KEY = environ.get("SECRET_KEY", "abc12345")
 JWT_ALGORITHM = environ.get("JWT_ALGORITHM", "HS256")
 
+class PostgresSettings:
+    def __init__(
+            self, engine:str, db: str, host: str, port: Union[int, str], 
+            user: str, password: str, echo: bool = False, pool_size: int = 10
+        ):
+        self.engine = engine
+        self.db = db
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.echo = echo
+        self.pool_size = pool_size
+
+    def __str__(self):
+        route=f"{self.host}:{self.port}/{self.db}"
+        credentials=f"{self.user}:{self.password}"
+        return f"{self.engine}://{credentials}@{route}"
+
+    def __repr__(self):
+        route=f"{self.host}:{self.port}/{self.db}"
+        credentials=f"{self.user}:{self.password}"
+        return f"{self.engine}://{credentials}@{route}"
+
+# Database settings
+application_db_settings = PostgresSettings(
+    POSTGRES_ENGINE, POSTGRES_DB, 
+    POSTGRES_HOST,  POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD,
+    POSTGRES_ECHO, POSTGRES_POOL_SIZE
+)
+application_db_uri = str(application_db_settings)
+
+test_db_settings = PostgresSettings(
+    POSTGRES_ENGINE, POSTGRES_DB_TEST, 
+    POSTGRES_HOST_TEST, POSTGRES_PORT_TEST, POSTGRES_USER, POSTGRES_PASSWORD,
+    POSTGRES_ECHO, POSTGRES_POOL_SIZE
+)
+test_db_uri = str(test_db_settings)
+
+class JWTSettings:
+    def __init__(self, secret_key: str, algorithm: str, access_token_expire_minutes: int):
+        self.secret_key = secret_key
+        self.algorithm = algorithm
+        self.access_token_expire_minutes = access_token_expire_minutes
+
+    def __str__(self):
+        content=f"{self.secret_key}, {self.algorithm}, {self.access_token_expire_minutes}"
+        return f"JWTSettings({content})"
+
+    def __repr__(self):
+        content=f"{self.secret_key}, {self.algorithm}, {self.access_token_expire_minutes}"
+        return f"JWTSettings({content})"
+
+# JWT settings
+jwt_settings = JWTSettings(SECRET_KEY, JWT_ALGORITHM, 60 * 24 * 7)
+
 # Project settings
 with open("pyproject.toml", "r") as f:
     config = toml.load(f)
-
 
 # Settings class
 class Settings(BaseSettings):
@@ -45,59 +110,15 @@ class Settings(BaseSettings):
     # Environment information: development, testing, production
     ENVIRONMENT: str = Field(ENVIRONMENT)
 
-    POSTGRES_DB: str = Field(POSTGRES_DB)
-    POSTGRES_HOST: str = Field(POSTGRES_HOST)
-    POSTGRES_PORT: Union[int, str] = Field(POSTGRES_PORT)
-    POSTGRES_USER: str = Field(POSTGRES_USER)
-    POSTGRES_PASSWORD: str = Field(POSTGRES_PASSWORD)
-
-    POSTGRES_ECHO: bool = Field(POSTGRES_ECHO)
-    POSTGRES_POOL_SIZE: int = Field(POSTGRES_POOL_SIZE)
-    ASYNC_POSTGRES_URI: Union[None, str] = None
-    ASYNC_POSTGRES_URI_TEST: Union[None, str] = None
+    # Postgres settings
+    POSTGRES_APPLICATION_SETTINGS: PostgresSettings = Field(application_db_settings)
+    POSTGRES_TEST_SETTINGS: PostgresSettings = Field(test_db_settings)
+    
+    ASYNC_POSTGRES_URI: str = Field(application_db_uri)
+    ASYNC_POSTGRES_URI_TEST: str = Field(test_db_uri)
 
     # JWT
-    SECRET_KEY: str = Field(SECRET_KEY)
-    JWT_ALGORITHM: str = Field(JWT_ALGORITHM)
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = (
-        60 * 24 * 7
-    )  # 60 minutes * 24 hours * 7 days = 7 days
-
-    @field_validator("ASYNC_POSTGRES_URI")
-    def assemble_db_connection(cls, v: Union[None, str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-
-        data = values.data
-
-        return str(
-            URL.create(
-                drivername="postgresql+asyncpg",
-                username=data.get("POSTGRES_USER"),
-                password=data.get("POSTGRES_PASSWORD"),
-                host=data.get("POSTGRES_HOST"),
-                database=data.get("POSTGRES_DB"),
-                port=int(data.get("POSTGRES_PORT")),
-            )
-        )
-    
-    @field_validator("ASYNC_POSTGRES_URI_TEST")
-    def assemble_test_db_connection(cls, v: Union[None, str], values: Dict[str, Any]) -> Any:
-        if isinstance(v, str):
-            return v
-
-        data = values.data
-
-        return str(
-            URL.create(
-                drivername="postgresql",
-                username=data.get("POSTGRES_USER"),
-                password=data.get("POSTGRES_PASSWORD"),
-                host=data.get("POSTGRES_HOST"),
-                database=data.get("POSTGRES_DB")+'_test',
-                port=int(data.get("POSTGRES_PORT")),
-            )
-        )
+    JWT_SETTINGS: JWTSettings = Field(jwt_settings)
 
     class Config(ConfigDict):
         case_sensitive = True
