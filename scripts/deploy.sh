c9cbf2b6ec51b454450035662f4e3b0fd10be592
Bruno Peixoto <brunolnetto@gmail.com>
2024-05-06 23:20:47 -0300

    config/ new scripts for build and deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+DOMAIN=${DOMAIN?Variable not set} \
+STACK_NAME=${STACK_NAME?Variable not set} \
+docker-compose \
+-f docker-compose.yml \
+config > docker-stack.yml
+
+docker-auto-labels docker-stack.yml
+
+docker stack deploy -c docker-stack.yml --with-registry-auth "${STACK_NAME?Variable not set}"
